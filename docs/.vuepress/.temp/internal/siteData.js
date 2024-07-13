cce8cf913ec9945398e7ed2592732d9e4a14b7c0
ZENGY27J <zexyzm@163.com>
2024-07-13 22:07:42 +0800

    7

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/vuepress-starter/\",\"lang\":\"en-US\",\"title\":\"ZengY's document\",\"description\":\"live and life\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"en-US\",\"title\":\"ZengY's document\",\"description\":\"live and life\",\"head\":[],\"locales\":{}}")
