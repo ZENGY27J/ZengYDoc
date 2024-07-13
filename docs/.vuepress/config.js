119a52870b1e64e573fc0339d153b1e7a88a4037
ZENGY27J <zexyzm@163.com>
2024-07-13 21:55:30 +0800

    5

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,13 +3,15 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  base:'/vuepress-starter/',
   lang: 'en-US',
 
   title: 'ZengY\'s document',
   description: 'live and life',
 
+  base: '/vuepress-starter/',
+
   theme: defaultTheme({
+
     logo: 'https://vuejs.press/images/hero.png',
  
     navbar: [
